87c22c81e66419a10b4ffb67d8a322240d1ea0f8
Andi Bašić <andi.basic10@gmail.com>
2018-04-23 21:41:06 +0200

    jela

---
+++ include/menu-menu.php
@@ -0,0 +1,24 @@
+<div class="header head">
+	<div class="container">
+		<div class="logo animated wow pulse" data-wow-duration="1000ms" data-wow-delay="500ms">
+			<h1><a href="index.php"><span>C</span><img src="images/oo.png" alt=""><img src="images/oo.png" alt="">kery</a></h1>
+		</div>
+		<div class="nav-icon">		
+			<a href="#" class="navicon"></a>
+				<div class="toggle">
+					<ul class="toggle-menu">
+						<li><a  href="index.php">Home</a></li>
+						<li><a class="active" href="menu.php">Menu</a></li>
+						<li><a  href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+			<script>
+			$('.navicon').on('click', function (e) {
+			  e.preventDefault();
+			  $(this).toggleClass('navicon--active');
+			  $('.toggle').toggleClass('toggle--active');
+			});
+			</script>
+		</div>
+	<div class="clearfix"></div>
+	</div></div>
---
+++ menu.php
@@ -0,0 +1,37 @@
+<!--A Design by W3layouts 
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include_once 'include/head.php'; ?>
+</head>
+<body>
+	<?php include_once 'include/menu-menu.php'; ?>		
+
+<!--content-->
+	<div class="menu">
+		<div class="container">
+			<div class="menu-top">
+				<div class="col-md-4 menu-left animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<h3>Menu</h3>
+					<label><i class="glyphicon glyphicon-menu-up"></i></label>
+					<span>There are many variations</span>
+				</div>
+				<div class="col-md-8 menu-right animated wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			
+		</div>
+	</div>
+<!--footer-->
+	<?php include_once 'include/footer.php'; ?>
+	
+
+</body>
+</html>
